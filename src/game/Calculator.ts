e8913d28adcd28eea3c1bcbbf12e910ce39a95fa
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-31 03:22:11 +0800

    Update Calculator.ts

--- src/game/Calculator.ts
+++ src/game/Calculator.ts
@@ -9,12 +9,12 @@ export class Calculator {
 
     if (block01.isAnyBAR && block02.isAnyBAR && block03.isAnyBAR) {
       // Any BAR combination
-      return 2;
+      return 1;
     }
 
     if (block01.isSevenOrCherry && block02.isSevenOrCherry && block03.isSevenOrCherry) {
       // Any Seven or Cherry combination
-      return 64;
+      return 1;
     }
 
     return 0;
@@ -22,14 +22,14 @@ export class Calculator {
 
   private getMultiplier(block: Block, position: Position) {
     if (block.isCherry) {
-      if (position === 'Top') return 2;
+      if (position === 'Top') return 1;
       if (position === 'Middle') return 1;
-      if (position === 'Bottom') return 3;
+      if (position === 'Bottom') return 1;
     }
 
     if (block.isSeven) {
       if (position === 'Top') return 1;
-      if (position === 'Middle') return 3;
+      if (position === 'Middle') return 1;
       if (position === 'Bottom') return 2;
     }
 
@@ -37,11 +37,11 @@ export class Calculator {
   }
 
   private getValue(block: Block) {
-    if (block.isBARx1) return 4;
-    if (block.isBARx2) return 8;
-    if (block.isBARx3) return 16;
-    if (block.isSeven) return 128;
-    if (block.isCherry) return 256;
+    if (block.isBARx1) return 0;
+    if (block.isBARx2) return 1;
+    if (block.isBARx3) return 2;
+    if (block.isSeven) return 1;
+    if (block.isCherry) return 1;
 
     return 0;
   }
